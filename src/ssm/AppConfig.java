b67e95e94d6cb34519a7423df80c61d8fdf96a7c
Mr Zhu <451218651@qq.com>
2018-04-16 15:14:55 +0800

    使用spring jdbc查询客户

--- src/ssm/AppConfig.java
+++ src/ssm/AppConfig.java
@@ -1,7 +1,15 @@
 package ssm;
 
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
@@ -10,6 +18,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration 
 @ComponentScan(basePackages = "ssm") 
 @EnableWebMvc
+@PropertySource("classpath:jdbc.properties")
 public class AppConfig extends WebMvcConfigurerAdapter {
 
 	@Override
@@ -17,5 +26,24 @@ public class AppConfig extends WebMvcConfigurerAdapter {
 		// 假设控制器返回"customers"，则做如下拼接确定jsp路径
 		// "/WEB-INF/jsp/" + "customers" + ".jsp"
 		registry.jsp("/WEB-INF/jsp/", ".jsp");
+	}
+	
+	@Bean
+	@Primary // 首选
+	public DataSource testDataSource(Environment env) { // 数据源的初始化依赖配置环境
+		String url = env.getProperty("jdbc.url");
+		String username = env.getProperty("jdbc.username");
+		String password = env.getProperty("jdbc.password");
+		String driverClass = env.getProperty("jdbc.driverClass");
+		
+		DriverManagerDataSource ds = new DriverManagerDataSource(url, username, password);
+		ds.setDriverClassName(driverClass);
+		return ds;
+	}
+	
+	
+	@Bean
+	public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+		return new JdbcTemplate(dataSource);
 	}
 }
--- src/ssm/dao/CustomerDaoImpl.java
+++ src/ssm/dao/CustomerDaoImpl.java
@@ -1,8 +1,12 @@
 package ssm.dao;
 
-import java.util.ArrayList;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
 
 import ssm.entity.Customer;
@@ -10,16 +14,31 @@ import ssm.entity.Customer;
 @Repository
 public class CustomerDaoImpl implements CustomerDao {
 
-	@Override
-	public List<Customer> findAll() {
-		List<Customer> customers = new ArrayList<>();
-		Customer zs = new Customer();
-		zs.setId(1L);
-		zs.setName("zhangsan");
-		zs.setAddress("hw");
-		zs.setVip(true);
-		customers.add(zs);
-		return customers;
+	private JdbcTemplate jdbcTemplate;
+	
+	@Autowired
+	public CustomerDaoImpl(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
 	}
 
+	@Override
+	public List<Customer> findAll() {
+		String sql = "SELECT id, name, address, vip FROM CUSTOMERS";
+		return jdbcTemplate.query(sql, new CustomerRowMapper());
+	}
+
+}
+
+class CustomerRowMapper implements RowMapper<Customer> {
+
+	@Override
+	public Customer mapRow(ResultSet resultSet, int arg1) throws SQLException {
+		Customer customer = new Customer();
+		customer.setId(resultSet.getLong(1));
+		customer.setName(resultSet.getString(2));
+		customer.setAddress(resultSet.getString(3));
+		customer.setVip(resultSet.getBoolean(4));
+		return customer;
+	}
+	
 }
