4df748892720062fe71a5e9d7a60656169045080
Mr Zhu <451218651@qq.com>
2018-04-27 16:39:55 +0800

    mybatis: Blog.posts对多关系映射

--- src/ssm/entity/Blog.java
+++ src/ssm/entity/Blog.java
@@ -1,11 +1,16 @@
 package ssm.entity;
 
+import java.util.List;
+
 public class Blog {
 	private Long id;
 	private String title;
 	
 	// 对一关系
 	private Author author;
+	
+	// 对多关系
+	private List<Post> posts;
 
 	public Long getId() {
 		return id;
@@ -31,10 +36,18 @@ public class Blog {
 		this.author = author;
 	}
 
+	public List<Post> getPosts() {
+		return posts;
+	}
+
+	public void setPosts(List<Post> posts) {
+		this.posts = posts;
+	}
+
 	@Override
 	public String toString() {
 		return "Blog [id=" + id + ", title=" + title + ", author=" + author
-				+ "]";
+				+ ", posts=" + posts + "]";
 	}
 	
 }
