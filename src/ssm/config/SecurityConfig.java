f7e8f0bb93f44e66f65aac3aeab5c0342e28a8b4
Mr Zhu <451218651@qq.com>
2018-05-02 10:45:10 +0800

    springsecurity: 基本配置（(过滤器 UserDetailsManager 授权检查)

---
+++ src/ssm/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package ssm.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Bean // spring security需要实现UserDetailsManager的bean来获得用户信息
+	public UserDetailsManager userDetailsManager() {
+		InMemoryUserDetailsManager um = new InMemoryUserDetailsManager();
+		um.createUser(User.withUsername("zhangsan").password("zhangsan").roles("USER").build());
+		um.createUser(User.withUsername("lisi").password("lisi").roles("ADMIN").build());
+		return um;
+	}
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// Fluent API
+		http.authorizeRequests()
+			.antMatchers("/admin/**").hasRole("ADMIN")
+			.antMatchers("/assets/**").permitAll()
+			.antMatchers("/**").authenticated()
+			.and()
+			.formLogin(); // 使用表单登录
+	}
+}
--- src/ssm/controller/CustomerController.java
+++ src/ssm/controller/CustomerController.java
@@ -37,7 +37,7 @@ public class CustomerController {
 	}
 
 	// 指定此控制器方法用来处理GET /customers
-	@RequestMapping(method = RequestMethod.GET, value = "/customers")
+	@RequestMapping(method = RequestMethod.GET, value = "/admin/customers")
 	public String findAll(Model model, 
 			@RequestParam(required = false, defaultValue = "1") int page) {
 		System.out.println("处理: GET /customers, page=" + page);
